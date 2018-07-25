af54a0a1f9dfa0d470c47345fa40d40f4125cc06
Guillermo <wpelmodesigner@gmail.com>
2018-07-26 02:10:58 +0800

    step 3 html update

--- library/js/common.js
+++ library/js/common.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 		if($('#nomineeShareHolder').is(':checked')) { 
 			$('.nominees-shareholder-input').prop('disabled', true);
 			$('.nominees-shareholder-input').parents('.custom-checkbox').hide();
-			$('.add-shareholder-btn').prop('disabled', true);
+			$('.add-shareholder-btn').hide();
 		} else {
 			$('.nominees-input').prop('disabled', false);
 			$('.nominees-input').parents('.custom-checkbox').show();
-			$('.add-shareholder-btn').prop('disabled', false);
+			$('.add-shareholder-btn').show();
 		}
 	});
 	
@@ -16,11 +16,11 @@ $(document).ready(function(){
 		if($('#selectShareHolder').is(':checked')) { 
 			$('.nominees-shareholder-input').prop('disabled', false);
 			$('.nominees-shareholder-input').parents('.custom-checkbox').show();
-			$('.add-shareholder-btn').prop('disabled', false);
+			$('.add-shareholder-btn').show();
 		} else {
 			$('.nominees-shareholder-input').prop('disabled', true);
 			$('.nominees-shareholder-input').parents('.custom-checkbox').hide();
-			$('.add-shareholder-btn').prop('disabled', true);
+			$('.add-shareholder-btn').hide();
 		}
 	});
 
@@ -28,24 +28,165 @@ $(document).ready(function(){
 		if($('#nomineeDirector').is(':checked')) { 
 			$('.nominees-director-input').prop('disabled', true);
 			$('.nominees-director-input').parents('.custom-checkbox').hide();
-			$('.add-director-btn').prop('disabled', true);
+			$('.add-director-btn').hide();
 		} else {
 			$('.nominees-director-input').prop('disabled', false);
 			$('.nominees-director-input').parents('.custom-checkbox').show();
-			$('.add-director-btn').prop('disabled', false);
+			$('.add-director-btn').show();
 		}
 	});
 	
 	$('#selectDirector').click(function() {
-		if($('#nomineeDirector').is(':checked')) { 
+		if($('#selectDirector').is(':checked')) { 
 			$('.nominees-director-input').prop('disabled', true);
 			$('.nominees-director-input').parents('.custom-checkbox').hide();
-			$('.add-director-btn').prop('disabled', true);
+			$('.add-director-btn').show();
 		} else {
 			$('.nominees-director-input').prop('disabled', false);
 			$('.nominees-director-input').parents('.custom-checkbox').show();
-			$('.add-director-btn').prop('disabled', false);
+			$('.add-director-btn').hide();
 		}
 	});
+	
+	
+	var shareHolderStr = "";
+	
+	shareHolderStr += '<div class="col-sm-12 border pt-2 mb-3">',
+		shareHolderStr += '<div class="row">',
+			shareHolderStr += '<div class="col-sm">',
+				shareHolderStr += '<h6 class="item-shareholder-title">Shareholder <span>0</span></h6>',
+			shareHolderStr += '</div>',
+			shareHolderStr += '<div class="col-sm text-right">',
+				shareHolderStr += '<div class="custom-control custom-checkbox my-1 mr-sm-2 shareholder-checkbox">',
+					shareHolderStr += '<input type="checkbox" class="custom-control-input nominees-shareholder-input" id="customControlInline3">',
+					shareHolderStr += '<label class="custom-control-label" for="customControlInline3">Nominee</label>',
+				shareHolderStr += '</div>',
+			shareHolderStr += '</div>',
+		shareHolderStr += '</div>',
+		shareHolderStr += '<div class="row">',
+			shareHolderStr += '<div class="col-sm">',
+			  shareHolderStr += '<div class="form-group">',
+				shareHolderStr += '<label for="exampleInputPassword1">Name</label>',
+				shareHolderStr += '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Name">',
+			  shareHolderStr += '</div>',
+			shareHolderStr += '</div>',
+			shareHolderStr += '<div class="col-sm">',
+			  shareHolderStr += '<div class="form-group">',
+				shareHolderStr += '<label for="exampleInputPassword1">Nationailty</label>',
+				shareHolderStr += '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nationality">',
+			  shareHolderStr += '</div>',
+			shareHolderStr += '</div>',
+			shareHolderStr += '<div class="col-sm">',
+			  shareHolderStr += '<div class="form-group">',
+				shareHolderStr += '<label for="exampleInputPassword1">Occupation</label>',
+				shareHolderStr += '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nationality">',
+			  shareHolderStr += '</div>',
+			shareHolderStr += '</div>',
+		shareHolderStr += '</div>',
+		
+		shareHolderStr += '<div class="row">',
+			shareHolderStr += '<div class="col-sm">',
+			  shareHolderStr += '<div class="form-group">',
+				shareHolderStr += '<label for="exampleInputPassword1">Address</label>',
+				shareHolderStr += '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Address">',
+			  shareHolderStr += '</div>',
+			shareHolderStr += '</div>',
+			shareHolderStr += '<div class="col-4">',
+			  shareHolderStr += '<div class="form-group">',
+				shareHolderStr += '<label for="exampleInputPassword1">Number of Shares</label>',
+				shareHolderStr += '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="No. of Shares">',
+			  shareHolderStr += '</div>',
+			shareHolderStr += '</div>',	
+		shareHolderStr += '</div>',
+	shareHolderStr += '</div>';
+	
+	
+	var directorStr = "";
+	
+	directorStr += '<div class="col-sm-12 border pt-2 mb-3">',
+		directorStr += '<div class="row">',
+			directorStr += '<div class="col-sm">',
+				directorStr += '<h6 class="item-director-title">Director <span>0</span></h6>',
+			directorStr += '</div>',
+			directorStr += '<div class="col-sm text-right">',
+				directorStr += '<div class="custom-control custom-checkbox my-1 mr-sm-2 director-checkbox">',
+					directorStr += '<input type="checkbox" class="custom-control-input nominees-shareholder-input" id="customControlInlineDirector3">',
+					directorStr += '<label class="custom-control-label" for="customControlInlineDirector3">Nominee</label>',
+				directorStr += '</div>',
+			directorStr += '</div>',
+		directorStr += '</div>',
+		directorStr += '<div class="row">',
+			directorStr += '<div class="col-sm">',
+			  directorStr += '<div class="form-group">',
+				directorStr += '<label for="exampleInputPassword1">Name</label>',
+				directorStr += '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Name">',
+			  directorStr += '</div>',
+			directorStr += '</div>',
+			directorStr += '<div class="col-sm">',
+			  directorStr += '<div class="form-group">',
+				directorStr += '<label for="exampleInputPassword1">Nationailty</label>',
+				directorStr += '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nationality">',
+			  directorStr += '</div>',
+			directorStr += '</div>',
+			directorStr += '<div class="col-sm">',
+			  directorStr += '<div class="form-group">',
+				directorStr += '<label for="exampleInputPassword1">Occupation</label>',
+				directorStr += '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nationality">',
+			  directorStr += '</div>',
+			directorStr += '</div>',
+		directorStr += '</div>',
+		
+		directorStr += '<div class="row">',
+			directorStr += '<div class="col-sm">',
+			  directorStr += '<div class="form-group">',
+				directorStr += '<label for="exampleInputPassword1">Address</label>',
+				directorStr += '<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Address">',
+			  directorStr += '</div>',
+			directorStr += '</div>',	
+		directorStr += '</div>',
+	directorStr += '</div>';
+	
+	//console.log(directorStr);
+	//var counter = 1;
+		
+	$('.add-shareholder-btn').click(function(){
+		$('#shareholder-content').after(shareHolderStr);	
+		
+		/* Title Counter */
+		$("h6.item-shareholder-title").each(function(i) {
+			$(this).find("span").text(++i);
+		});
+		
+		/* Nominees Counter */
+		$(".shareholder-checkbox").each(function(i) {
+			$(this).find("input").attr('id', 'customControlInline' + ++i);
+		});
+		
+		$(".shareholder-checkbox").each(function(i) {
+			$(this).find("label").attr('for', 'customControlInline' + ++i);
+		});
+		
+	});
+	
+	
+	$('.add-director-btn').click(function(){
+		$('#directors-content').after(directorStr);	
+		
+		/* Title Counter */
+		$("h6.item-director-title").each(function(i) {
+			$(this).find("span").text(++i);
+		});
+		
+		/* Nominees Counter */
+		$(".director-checkbox").each(function(i) {
+			$(this).find("input").attr('id', 'customControlInlineDirector' + ++i);
+		});
+		
+		$(".director-checkbox").each(function(i) {
+			$(this).find("label").attr('for', 'customControlInlineDirector' + ++i);
+		});
+		
+	});
+	
 	
 });
